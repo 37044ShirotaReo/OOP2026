8bd732c6499028b1781b6dfaae27df272f2b1082
白田 怜央 <oibc37044@ojs.ac.jp>
2026-04-23 10:19:15 +0900

    静的クラスへ変更

--- Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DistanceConverter {
-    public class FeetConverter {
+namespace DistanceConverter
+{
+    public class FeetConverter
+    {
+        private const double ratio = 0.3048;   //定数
 
         // メートルからフィートを求める
-        public double FromMeter(double meter) {
-            return meter / 0.3048;
+        public static double FromMeter(double meter)
+        {
+            return meter / ratio;
         }
 
         //フィートからメートルを求める
-        public double ToMeter(double feet) {
-            return feet * 0.3048;
+        public static double ToMeter(double feet)
+        {
+            return feet * ratio;
         }
     }
 }
