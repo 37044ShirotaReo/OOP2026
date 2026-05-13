2d56f53541b9efaa90ba94a40c9393acae38e51c
白田 怜央 <oibc37044@ojs.ac.jp>
2026-05-13 11:20:16 +0900

    2.2 完成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    public class InchConverter {
+
+        private static readonly double ratio = 0.0254;   //定数
+
+        public static double ToMeter(double inch) {
+            return inch * ratio;
+        }
+    }
+}
