8152f07ea501d45c8de08f69bd3b1e44569bd4ca
白田 怜央 <oibc37044@ojs.ac.jp>
2026-05-13 10:42:38 +0900

    データ入力対応

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -3,21 +3,51 @@ namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
 
+            var songs = new List<Song>();
+
+            Console.WriteLine("***** 曲の登録 *****");
+
+            while(true) {
+                //"曲名:"を出力
+                Console.Write("曲名:");
+                string? title = Console.ReadLine();
+
+                if (title.Equals("end", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                //"アーティスト名:"を出力
+                Console.Write("アーティスト名:");
+                string? artistName = Console.ReadLine();
+
+                //"演奏時間(秒)を出力
+                Console.Write("演奏時間(秒):");
+                int length = int.Parse(Console.ReadLine());
+
+
+                Song song = new Song(title, artistName, length);
+
+
+
+                songs.Add(song);
+
+                Console.WriteLine();
+            }
+            
             // 2.1.3
-            var songs = new Song[] {
-                new Song("Let it be", "The Beatles", 243),
-                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-                new Song("Close To You", "Carpenters", 276),
-                new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273),
-            };
+            //var songs = new Song[] {
+            //    new Song("Let it be", "The Beatles", 243),
+            //    new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+            //    new Song("Close To You", "Carpenters", 276),
+            //    new Song("Honesty", "Billy Joel", 231),
+            //    new Song("I Will Always Love You", "Whitney Houston", 273),
+            //};
             PrintSongs(songs);
         }
         //Mainメソッド内の　PrintSong(song);　をクリックして
         //Alt + Enterを押してメソッドを生成するを選択すると、
         //以下のメソッドが自動的に作成される
         // 2.1.4
-        private static void PrintSongs(Song[] songs) {
+        private static void PrintSongs(IEnumerable<Song> songs) {
             foreach (var song in songs) {
                 var minutes = song.Length / 60;
                 var seconds = song.Length % 60;
