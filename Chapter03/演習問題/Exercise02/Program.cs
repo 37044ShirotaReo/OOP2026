555386f1b54e692de56f73b74538035b9f4c365e
白田 怜央 <oibc37044@ojs.ac.jp>
2026-05-21 12:06:25 +0900

    問題3.2.3完成

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -37,7 +37,10 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_3(List<string> cities) {
-            
+            var selected = cities.Where(s => s.Contains('o')).ToArray();
+            foreach (var name in selected) {
+                Console.WriteLine(name);
+            }
         }
 
         private static void Exercise2_4(List<string> cities) {
