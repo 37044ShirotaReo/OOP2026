bfa25ccce439358124dd10834da899ce917bd092
白田 怜央 <oibc37044@ojs.ac.jp>
2026-06-04 09:52:07 +0900

    演習問題5.1.3完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -18,10 +18,19 @@ namespace Exercise01 {
         }
 
         //5.1.2
+        //設定されている西暦が21世紀か判定する
+        //Yearが2001～2100年の間ならtrue、それ以外ならfalseを返す
         public bool Is21Century => 2001 <= Year && Month <= 2100;
 
         //5.1.3
-        
+        public YearMonth AddOneMonth() {
+            if (Month == 12) {
+                return new YearMonth(Year + 1, 1);
+            }
+            return new YearMonth(Year, Month + 1);
+        }
+
+        //5.1.4
         
 
 
