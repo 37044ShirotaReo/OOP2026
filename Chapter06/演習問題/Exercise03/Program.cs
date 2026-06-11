f7cb0a375cccb40ce7acbee34b2ba2054aeec113
白田 怜央 <oibc37044@ojs.ac.jp>
2026-06-11 11:46:09 +0900

    Revert "配列を用いた集計"
    This reverts commit e0b90b63c51f4cdbf904fe28eb433e83bd9d044a.

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -57,28 +57,24 @@ namespace Exercise03 {
             var str = text.ToLower().Replace(" ", "");
 
             //辞書(ディクショナリ)を使った集計
-            var alphDicCount = Enumerable.Range('a', 26).ToDictionary(num => ((char)num), num => 0);
+            //var alphDicCount = Enumerable.Range('a', 26).ToDictionary(num => ((char)num).ToString(), num => 0);
 
-            //var dict = new SortedDictionary<char, int>();
+            var dict = new SortedDictionary<char, int>();
             foreach (var c in str) {
-                alphDicCount[c]++;
+                if (dict.ContainsKey(c))
+                    dict[c]++;      //登録されている場合
+                else
+                    dict[c] = 1;　　//未登録の場合
             }
-
-            foreach (var word in alphDicCount) {
+            foreach (var word in dict) {
                 Console.WriteLine(word.Key + ":" + word.Value);
             }
 
+
             //配列を用いた集計
-            var array = Enumerable.Repeat(0, 26).ToArray();
-            foreach (var alph in str) {
-                array[alph - 'a']++;
-            }
-            for (char ch = 'a'; ch <= 'z'; ch++) {
-                Console.WriteLine($"{ch}:{array[ch - 'a']}");
-            }
+
 
             //'a'から順にカウントして集計
-
         }
     }
 }
