e76ad14185c33f0e6f9f8978b378ca13b06754d5
白田 怜央 <oibc37044@ojs.ac.jp>
2026-06-11 10:49:40 +0900

    辞書(ディクショナリ)を使った集計その１

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -19,33 +19,62 @@ namespace Exercise03 {
             Console.WriteLine("6.3.5");
             Exercise5(text);
 
-            Console.WriteLine("6.3.99");
+            Console.WriteLine("6.3.6");
             Exercise6(text);
             #endregion
         }
 
         private static void Exercise1(string text) {
-            
+            int count = text.Count(c => c == ' ');
+            Console.WriteLine($"空白数：{count}");
         }
 
         private static void Exercise2(string text) {
-            
+            Console.Write("検索：");
+            var search = Console.ReadLine();
+            Console.Write("置換：");
+            var replace = Console.ReadLine();
+
+            var replaced = text.Replace(search, replace);
+            Console.WriteLine(replaced);
         }
 
         private static void Exercise3(string text) {
-            
+            //やらなくてよい
         }
 
         private static void Exercise4(string text) {
-            
+            var count = text.Split(' ').Length;
+            Console.WriteLine($"単語数：{count}");
         }
 
         private static void Exercise5(string text) {
-            
+           text.Split(' ').Where(s => s.Length <= 4).ToList().ForEach(Console.WriteLine);
         }
 
+        //アルファベットの数をカウントして表示する
         private static void Exercise6(string text) {
-            
+            var str = text.ToLower().Replace(" ", "");
+
+            //辞書(ディクショナリ)を使った集計
+            //var alphDicCount = Enumerable.Range('a', 26).ToDictionary(num => ((char)num).ToString(), num => 0);
+
+            var dict = new SortedDictionary<char, int>();
+            foreach (var c in str) {
+                if (dict.ContainsKey(c))
+                    dict[c]++;      //登録されている場合
+                else
+                    dict[c] = 1;　　//未登録の場合
+            }
+            foreach (var word in dict) {
+                Console.WriteLine(word.Key + ":" + word.Value);
+            }
+
+
+            //配列を用いた集計
+
+
+            //'a'から順にカウントして集計
         }
     }
 }
